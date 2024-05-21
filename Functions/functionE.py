b4bf81a4e3e72875fe3a01b446eac893ce70686d
Aron van der Hofstad <aron.hofstad@live.nl>
2024-05-21 21:25:37 +0200

    Added FunctionE

---
+++ Functions/functionE.py
@@ -0,0 +1,17 @@
+#Filler text about that this function will move around
+
+#More Filler Text
+
+def functionE():
+    #Some Filler Text
+    print("Done")
+    #More filler text
+    
+    print("More details")
+    
+    print("Too Long")
+    
+    print("This is the end")
+    #Filler Text end of function
+    
+#More Filler Text afterwards
